0a936d48999eee8c295cfef71e505f5b4da7e39a
johnsmccain <johnsdanlami@gmail.com>
2026-05-27 12:39:33 +0100

    feat: add asset-escrow-v3, reward-unlocker, guild-progress contracts and split metadata sidebar UI

---
+++ frontend/src/components/SplitMetadata/index.ts
@@ -0,0 +1,12 @@
+export { SplitMetadataLayout } from "./SplitMetadataLayout";
+export { MetadataSection } from "./MetadataSection";
+export { StateDisplay } from "./StateDisplay";
+
+export type {
+  SplitMetadataLayoutProps,
+  MetadataSectionProps,
+  MetadataField,
+  MetadataSection,
+  LoadingSkeletonProps,
+  StateDisplayProps,
+} from "./types";
